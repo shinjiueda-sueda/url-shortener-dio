a696f4fede5137fabae783edd4778372209976bb
sueda <shinjiueda.sueda@gmail.com>
2021-11-16 23:32:44 -0300

    Commit Inicial

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+API_URL: 'http://localhost:5000',
+MONGO_CONNECTION:
+'mongodb+srv://<user>:<password>@url-shortener-dio.letvs.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+}
